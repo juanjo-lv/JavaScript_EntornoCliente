27eaddfb143f30f0e3ef468a9053b0c0f88e3145
juanjo-lv <juanjoselopezvega94@gmail.com>
2021-02-24 22:54:38 +0100

    Ejercicio 1 completado

--- Practica_JuanJose_Lopez_Vega/Ejercicio1/scripts.js
+++ Practica_JuanJose_Lopez_Vega/Ejercicio1/scripts.js
@@ -8,30 +8,18 @@ function calcularBilletes() {
 
     boton.addEventListener("click",()=>{
         var cantidad = recogerDato.value;
-        var billymonedas ={
-            500 : 0,
-            200 : 0,
-            100 : 0,
-            50  : 0,
-            20  : 0,
-            10  : 0,
-            5   : 0,
-            2   : 0,
-            1   : 0,
-            0.5 : 0,
-            0.2 : 0,
-            0.1 : 0,
-            0.05: 0,
-            0.02: 0,
-            0.01: 0
-        }
         var tiposbilletes = [500,200,100,50,20,10,5,2,1,0.5,0.2,0.1,0.05,0.02,0.01];
         function operar(cantidad) {
             for(i=0;i<tiposbilletes.length;i++){
-                
+                let numbilletes = Math.trunc(cantidad / tiposbilletes[i]);
+                cantidad = cantidad -(numbilletes * tiposbilletes[i]);
+                if(i<=6){
+                    document.write("Billetes de "+tiposbilletes[i]+"€ :"+numbilletes+"<br>");
+                }else{
+                    document.write("Monedas de "+tiposbilletes[i]+"€ :"+numbilletes+"<br>");
+                }
             }
         }
-      
-
+       operar(cantidad);
     })
 }
