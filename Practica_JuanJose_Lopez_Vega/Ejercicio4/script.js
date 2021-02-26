a518dc3cf32715ee2be49621d5d6408358d57667
juanjo-lv <juanjoselopezvega94@gmail.com>
2021-02-26 20:17:38 +0100

    picture

--- Practica_JuanJose_Lopez_Vega/Ejercicio4/script.js
+++ Practica_JuanJose_Lopez_Vega/Ejercicio4/script.js
@@ -1,3 +1,52 @@
 document.addEventListener("DOMContentLoaded",()=>{
-
+    app();
 });
+function app() {
+    var insert = document.getElementById("insertar");
+    var color = document.getElementById("color");
+    var img = document.getElementById("imagen");
+    var borrar = document.getElementById("borrar");
+
+    insert.addEventListener("click",insertar);
+    color.addEventListener("click",cambiarColor);
+    img.addEventListener("click",insertarImagen);
+    borrar.addEventListener("click",borrarTodo);
+
+
+
+    function insertar() {
+        var parrafo = document.createElement("p");
+        var texto = document.createTextNode(document.getElementById("texto").value);
+
+        parrafo.appendChild(texto);
+
+        var contenedor = document.querySelector(".contenedor")
+        contenedor.appendChild(parrafo)
+    }
+    function cambiarColor() {
+        let cont = document.querySelector("p:last-of-type");
+        cont.setAttribute("class","miClase");
+
+    }
+    function insertarImagen() {
+        var imagen = document.createElement("img");
+        var ruta = "img/"
+        ruta += prompt("Introduce el nombre de la imagen");
+        imagen.src = ruta;
+
+        var contenedor = document.querySelector(".contenedor")
+       
+        setTimeout(() => {
+            contenedor.appendChild(imagen);
+        }, 3000);
+        
+    }
+    function borrarTodo() {
+        var contenedor = document.querySelector(".contenedor")
+        var hijo = contenedor.childNodes;
+        for(let i=0; i=hijo.length;i++){
+            contenedor.removeChild(contenedor.lastChild);
+        }
+        
+    }
+}
